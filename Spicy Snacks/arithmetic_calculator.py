3fb16a426e30e42c3affcb307c257ebdfc73cdfc
Grace Olayemi <olayemigrace29@gmail.com>
2026-05-09 13:12:58 +0100

    Task done

---
+++ Spicy Snacks/arithmetic_calculator.py
@@ -0,0 +1,20 @@
+num_one = float(input("Enter the first value: "))
+num_two = float(input("Enter the Second value: "))
+operator = input("Enter the Arithmetic Operator You want to make use of: ")
+add = num_one + num_two
+subtract = num_one - num_two
+multiply = num_one * num_two
+divide = num_one / num_two
+
+if operator == "+":
+    print(add)
+if operator == "-":
+    print(subtract)
+if operator == "*":
+    print(multiply)
+if operator == "/":
+    print(divide)
+    
+
+
+
---
+++ Spicy Snacks/password_strength.py
@@ -0,0 +1,11 @@
+password = input("Enter your preferred Password: ") 
+length =len(password)
+if length >= 6 and length <= 10:
+    print("Medium")
+elif length < 6:
+    print("Weak")    
+elif length > 10:
+    print("Strong")    
+elif length < 1:
+    print("Invalid")    
+    
